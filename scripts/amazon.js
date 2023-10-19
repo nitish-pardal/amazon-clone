b21d28798ec911c03a91499bbfc3fb484d83b1c3
nitish-pardal <nitishpardal@gmail.com>
2023-10-19 20:09:22 +0530

    19-10-2023

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -90,8 +90,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
 
    document.querySelector('.js-cart-quantity').innerHTML =cartQuantity;
 
-   console.log(cartQuantity);
-   console.log(cart);
   });
 
  });

500151a5e23d1438585958022991149e89ce397f
nitish-pardal <nitishpardal@gmail.com>
2023-10-20 17:22:48 +0530

    20-10-2023

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'))
+|| [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -9,6 +10,9 @@ export let cart = [
   }
 ];
 
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
 
@@ -28,6 +32,8 @@ export function addToCart(productId){
      quantity:1
    });
   }
+
+  saveToStorage();
 }
 
 
@@ -39,4 +45,6 @@ export function removeFromCart(productId){
     }
   });
   cart = newCart;
+
+  saveToStorage();
 }

ba6c6eef03dbb8a5c8af06273ec1d9e3c03888c3
nitish-pardal <nitishpardal@gmail.com>
2023-10-20 17:08:31 +0530

    20-10-2023

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart ,removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 let cartSummaryHTML="";
@@ -36,7 +36,7 @@ cartSummaryHTML +=
               <span class="update-quantity-link link-primary">
                 Update
               </span>
-              <span class="delete-quantity-link link-primary">
+              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -91,4 +91,12 @@ cartSummaryHTML +=
   `;
 });
 document.querySelector('.js-order-summary').innerHTML =cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) =>{
+  link.addEventListener('click',()=>{
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+  })
+});
 

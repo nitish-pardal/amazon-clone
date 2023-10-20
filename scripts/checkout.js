bf04fa72479fb9abce41753cb38651204c336a16
nitish-pardal <nitishpardal@gmail.com>
2023-10-20 17:14:32 +0530

    20-10-2023

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((cartItem)=>{
 
 cartSummaryHTML +=
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -96,7 +97,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) =>{
   link.addEventListener('click',()=>{
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+     const container = document.querySelector(`.js-cart-item-container-${productId}`);
+     
+     container.remove();
   })
 });
 

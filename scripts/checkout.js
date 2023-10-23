2597937cd03c1e29b94386465afd820998c500e1
nitish-pardal <nitishpardal@gmail.com>
2023-10-24 00:07:08 +0530

    24-10-2023

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
+import {hello} from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { calculateCartQuantity, cart ,removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";

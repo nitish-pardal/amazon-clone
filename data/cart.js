96080849c64e7dd9b8ccacd6c11a904ddea828e1
nitish-pardal <nitishpardal@gmail.com>
2023-10-24 11:54:18 +0530

    24-10-2023

--- data/cart.js
+++ data/cart.js
@@ -3,11 +3,13 @@ export let cart = JSON.parse(localStorage.getItem('cart'))
 || [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId:'1'
   },
   {
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId : '2'
   }
 ];
 
@@ -22,6 +24,8 @@ export function addToCart(productId){
   const quantity = Number(document.querySelector(`.js-product-quantity-${productId}`).value);
   let matchingItem;
 
+  console.log(quantity);
+
   cart.forEach((cartitem)=>{      // all the items in the cart
    if(cartitem.productId === productId){
      matchingItem = cartitem; 
@@ -34,7 +38,8 @@ export function addToCart(productId){
   }else{
    cart.push({
       productId,
-      quantity
+      quantity :quantity,
+      deliveryOptionId: '1'
    });
   }
 
